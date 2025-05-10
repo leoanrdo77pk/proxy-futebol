452d2f897b752f068e38d489b29cbfc757d19548
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-10 12:40:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-const TARGET = 'https://futebolaovivogratishd.com.br/';
+const TARGET = '';
 
 app.use('/futebol', (req, res) => {
   const url = TARGET + req.url;
