e2e2943d4150b39e3923cdb8aa0e0c4e200a5761
leoanrdo77pk <suporte.purofut@gmail.com>
2025-05-10 12:44:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const request = require('request');
-const app = express();
 
-const TARGET = '';
-
-app.use('/futebol', (req, res) => {
-  const url = TARGET + req.url;
-  req.pipe(request({ url, headers: { 'Referer': TARGET } })).pipe(res);
-});
-
-module.exports = app;
